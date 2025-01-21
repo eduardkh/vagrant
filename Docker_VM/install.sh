0986d2a503ef5fff864a65a404c99b15867bacbe
Eduard Khiaev <eddietheone1984@gmail.com>
2025-01-21 20:23:13 +0200

    Docker_VM

--- Docker_VM/install.sh
+++ Docker_VM/install.sh
@@ -1,8 +1,13 @@
-sudo apt-get remove docker docker-engine docker.io containerd runc
-echo "apt-get remove docker docker-engine docker.io containerd runc"
+echo "remove previous iterations of Docker if exists"
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+echo "remove previous iterations of Docker if exists - Done"
+
+# https://docs.docker.com/engine/install/ubuntu/#install-using-the-convenience-script
+echo "install Docker with convenience script"
 curl -fsSL https://get.docker.com -o get-docker.sh
-echo "curl -fsSL https://get.docker.com -o get-docker.sh"
 sudo sh get-docker.sh
-echo "sh get-docker.sh"
+echo "install Docker with convenience script - Done"
+
+echo "install pip3"
 sudo apt install python3-pip -y
-echo "apt install python3-pip -y"
+echo "install pip3 - Done"
