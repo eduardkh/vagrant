ee9e727b9888df3d43bde782458d499d0b4c148c
Eduard Khiaev <eddietheone1984@gmail.com>
2025-01-22 08:47:19 +0200

    docker/completions

--- Docker_VM_vmware/post_install.sh
+++ Docker_VM_vmware/post_install.sh
@@ -1,2 +1,5 @@
+echo "configure docker"
 sudo usermod -aG docker $USER
-echo "sudo usermod -aG docker \$USER"
+mkdir -p ~/.docker/completions
+docker completion zsh > ~/.docker/completions/_docker
+echo "configure docker - Done"
