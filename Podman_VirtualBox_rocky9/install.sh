78568054e164bb7236164d9e292e204b68e2393b
Eduard Khiaev <eddietheone1984@gmail.com>
2025-09-05 14:40:35 +0300

    Podman_VirtualBox_rocky9

---
+++ Podman_VirtualBox_rocky9/install.sh
@@ -0,0 +1,20 @@
+# echo "==>Updating the System"
+# sudo dnf update -y
+
+echo "==>install podman"
+sudo dnf install podman -y
+
+echo "==>Enable podman service"
+sudo systemctl enable --now podman
+
+echo "==>Check podman version"
+podman --version
+
+echo "==>Install Git and Make"
+sudo dnf install git -y
+sudo dnf install make -y
+
+echo "==>Install zsh"
+git clone https://github.com/eduardkh/linux.git
+cd linux/install-zsh/
+make rocky
