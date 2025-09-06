605be7a57da4199ea14a527512ad34cb6893a58c
Eduard Khiaev <eddietheone1984@gmail.com>
2025-09-06 12:33:40 +0300

    install helm

--- Podman_VMware_rocky9/install.sh
+++ Podman_VMware_rocky9/install.sh
@@ -10,14 +10,17 @@ sudo systemctl enable --now podman
 echo "==>Check podman version"
 podman --version
 
-echo "==>Install Git and Make"
-sudo dnf install git -y
-sudo dnf install make -y
+echo "==>Install Helm"
+sudo dnf install helm -y
 
 echo "==>Install Node.js"
 sudo dnf module reset nodejs -y
 sudo dnf module enable nodejs:20 -y        # or :22 if available
 sudo dnf install -y nodejs
+
+echo "==>Install Git and Make"
+sudo dnf install git -y
+sudo dnf install make -y
 
 echo "==>Install zsh"
 git clone https://github.com/eduardkh/linux.git
